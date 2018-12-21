5250543a79d63fc4a000fcb829d4e91addfd81c3
Tom Power <tpower21@gmail.com>
2018-12-21 14:37:55 +0000

    Fix template case and tidy up and xmas theme

--- src/main/kotlin/se.tp21.http4k/TemplateServer.kt
+++ src/main/kotlin/se.tp21.http4k/TemplateServer.kt
@@ -1,31 +1,36 @@
 package se.tp21.http4k
 
-import org.http4k.core.*
+import org.http4k.core.Method.GET
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.OK
+import org.http4k.core.then
 import org.http4k.filter.ServerFilters
 import org.http4k.routing.RoutingHttpHandler
 import org.http4k.routing.bind
 import org.http4k.routing.routes
-import org.http4k.server.Http4kServer
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
 import org.http4k.template.HandlebarsTemplates
 import org.http4k.template.ViewModel
 
-val render = HandlebarsTemplates().HotReload("src/main/resources")
+private data class Person(val name: String, val age: Int) : ViewModel
 
-data class Person(val name: String, val age: Int) : ViewModel
+private fun Request.person(): Person =
+    Person(
+        name = query("name")!!,
+        age = query("age")!!.toInt()
+    )
 
-private fun Request.person(): Person {
-    val name = query("name")!!
-    val age = query("age")!!.toInt()
-    return Person(name, age)
-}
+private fun ViewModel.render() = HandlebarsTemplates().HotReload("src/main/resources")(this)
 
-fun TemplateApp(): RoutingHttpHandler =
+val templateApp: RoutingHttpHandler =
     ServerFilters.CatchAll().then(
         routes(
-            "/person" bind Method.GET to { Response(Status.OK).body(render(it.person())) }
+            "/person" bind GET to {
+                Response(OK).body(it.person().render())
+            }
         )
     )
 
-fun TemplateServer(port: Int) = TemplateApp().asServer(Jetty(port))
+fun TemplateServer(port: Int) = templateApp.asServer(Jetty(port))
--- src/test/kotlin/se/tp21/http4k/TemplateServerKtTest.kt
+++ src/test/kotlin/se/tp21/http4k/TemplateServerKtTest.kt
@@ -3,20 +3,19 @@ package se.tp21.http4k
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.contains
 import com.natpryce.hamkrest.equalTo
-import org.http4k.core.Method
+import org.http4k.core.Method.GET
 import org.http4k.core.Request
-import org.http4k.core.Status
+import org.http4k.core.Status.Companion.OK
 import org.junit.jupiter.api.Test
 
 internal class TemplateServerKtTest {
 
     @Test
-    fun `returns a person with the right name and age`() {
-        TemplateApp().invoke(Request(Method.GET, "/person?name=bob&age=45")).run {
-            assertThat(status, equalTo(Status.OK))
-            assertThat(body.toString(), contains("name: bob".toRegex()))
-            assertThat(body.toString(), contains("age: 45".toRegex()))
+    fun `TemplateApp renders a person from a query string`() {
+        templateApp(Request(GET, "/person?name=santa&age=100")).run {
+            assertThat(status, equalTo(OK))
+            assertThat(body.toString(), contains("name: santa age: 100".toRegex()))
         }
     }
-    
+
 }
